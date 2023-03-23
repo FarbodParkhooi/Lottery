eedc863a6290facfb347fa32b234fa114b29b0de
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-03-23 22:59:55 +0330

    Create all Label(s)

--- Lottery.py
+++ Lottery.py
@@ -5,6 +5,11 @@ SelectNumbers=Tk()
 version = 1.2
 # Create Label(s) and Button(s)
 LabelOne = Label(SelectNumbers, text='Number 1: ', bg='#030314', fg='white')
+LabelTwo = Label(SelectNumbers, text='Number 2: ', bg='#030314', fg='white')
+LabelThree = Label(SelectNumbers, text='Number 3: ', bg='#030314', fg='white')
+LabelFour = Label(SelectNumbers, text='Number 4: ', bg='#030314', fg='white')
+LabelFive = Label(SelectNumbers, text='Number 5: ', bg='#030314', fg='white')
+LabelSix = Label(SelectNumbers, text='Number 6: ', bg='#030314', fg='white')
 # Window options: 
 SelectNumbers.config(bg='#030314')
 SelectNumbers.resizable(False,False)
