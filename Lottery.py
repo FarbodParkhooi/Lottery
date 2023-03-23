ca88ccb8ad019ec353b73af173af4097ded47257
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-03-23 23:08:45 +0330

    edit app

--- Lottery.py
+++ Lottery.py
@@ -2,14 +2,14 @@
 from tkinter import *
 # Create values
 SelectNumbers=Tk()
-version = 1.2
+version=1.2
 # Create Label(s) and Button(s)
-LabelOne = Label(SelectNumbers, text='Number 1: ', bg='#030314', fg='white')
-LabelTwo = Label(SelectNumbers, text='Number 2: ', bg='#030314', fg='white')
-LabelThree = Label(SelectNumbers, text='Number 3: ', bg='#030314', fg='white')
-LabelFour = Label(SelectNumbers, text='Number 4: ', bg='#030314', fg='white')
-LabelFive = Label(SelectNumbers, text='Number 5: ', bg='#030314', fg='white')
-LabelSix = Label(SelectNumbers, text='Number 6: ', bg='#030314', fg='white')
+LabelOne=Label(SelectNumbers,text='Number 1: ',bg='#030314',fg='white')
+LabelTwo=Label(SelectNumbers,text='Number 2: ',bg='#030314',fg='white')
+LabelThree=Label(SelectNumbers,text='Number 3: ',bg='#030314',fg='white')
+LabelFour=Label(SelectNumbers,text='Number 4: ',bg='#030314',fg='white')
+LabelFive=Label(SelectNumbers,text='Number 5: ',bg='#030314',fg='white')
+LabelSix=Label(SelectNumbers,text='Number 6: ',bg='#030314',fg='white')
 # .pack(s) |and| .place(s)
 # Window options: 
 SelectNumbers.config(bg='#030314')
