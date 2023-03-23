d115b13ce0c0b7ef477caa160d875540f05971b5
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-03-23 23:07:35 +0330

    add # .pack(s) |and| .place(s)

--- Lottery.py
+++ Lottery.py
@@ -10,6 +10,7 @@ LabelThree = Label(SelectNumbers, text='Number 3: ', bg='#030314', fg='white')
 LabelFour = Label(SelectNumbers, text='Number 4: ', bg='#030314', fg='white')
 LabelFive = Label(SelectNumbers, text='Number 5: ', bg='#030314', fg='white')
 LabelSix = Label(SelectNumbers, text='Number 6: ', bg='#030314', fg='white')
+# .pack(s) |and| .place(s)
 # Window options: 
 SelectNumbers.config(bg='#030314')
 SelectNumbers.resizable(False,False)
